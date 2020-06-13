073adc058e470b9f94ac634d3b26056e24722757
Ron <Ron@10.0.0.10>
2020-06-13 22:02:31 +0300

    Before Mvc

---
+++ TestProjectForGit/src/Controller/Controller.java
@@ -0,0 +1,12 @@
+package Controller;
+
+
+public class Controller {
+	private  theModel;
+	private View theView;
+	
+	public Controller(CinemaHall m, CinemaHallViewable v) {
+		theModel = m;
+		theView = v;
+
+}
